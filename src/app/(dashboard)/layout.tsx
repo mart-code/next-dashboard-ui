f73b368f93192280d46d56a7a36800648dfdde52
Ayanfeoluwa <aamadewoyin@gmail.com>
2026-04-23 01:16:37 +0100

    Lama school starting

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,14 @@
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen flex">
+      {/* LEFT */}
+      <div className=""></div>
+      {/* RIGHT */}
+      <div className=""></div>
+    </div>
+  );
+}
